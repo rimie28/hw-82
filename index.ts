9bda4aa3f9ade6c4fc6a69b5ebe1004bbd35854e
Meerim Maratova <maratovameerim@gmail.com>
2025-01-09 19:14:36 +0600

    added to index file/done

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import artistsRouter from "./routers/artists";
 import albumsRouter from "./routers/albums";
 import tracksRouter from "./routers/tracks";
+import usersRouter from "./routers/users";
+import trackHistoryRouter from "./routers/track_history";
 
 const app = express();
 app.use(express.json());
@@ -11,6 +13,9 @@ const port = 8000;
 app.use('/artists', artistsRouter);
 app.use('/albums', albumsRouter);
 app.use('/tracks', tracksRouter);
+app.use('/users', usersRouter);
+app.use('/track_history', trackHistoryRouter);
+
 
 mongoose
     .connect('mongodb://localhost:27017/hw-82')
